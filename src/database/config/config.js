9ce582eee8ffec2443d0b40053f1f7e5ecdcfb49
joanamds <joanamdsantos1@gmail.com>
2024-03-06 19:32:03 -0300

    :sparkles: feat: tentando modificar o vercel-build para ver se funciona

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -15,7 +15,7 @@ if (process.env.POSTGRES_URL) {
   options.url = process.env.POSTGRES_URL;
 } else {
   options.host = process.env.POSTGRES_HOST || 'localhost';
-  options.port = process.env.POSTGRES_PORT || '5432';
+  // options.port = process.env.POSTGRES_PORT || '5432';
   options.database = process.env.POSTGRES_DATABASE || 'postgres';
   options.username = process.env.POSTGRES_USER || 'postgres';
   options.password = process.env.POSTGRES_PASSWORD || 'senhaDoDB';
