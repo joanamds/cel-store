922d691b29a015fb89ee4170210ad63ff3f3404d
joanamds <joanamdsantos1@gmail.com>
2024-03-07 15:35:22 -0300

    :sparkles: feat: tentando mais uma vez

--- src/api/products/get.js
+++ src/api/products/get.js
@@ -1,11 +1,12 @@
 const ProductController = require('../../database/controllers/ProductController');
-const { validateToken } = require('../../database/auth/validateJWT');
+// const { validateToken } = require('../../database/auth/validateJWT');
 
-module.exports = async (req, res, next) => {
+module.exports = async (req, res) => {
   try {
-    validateToken(req, res, next);
+    // validateToken(req, res, next);
     await ProductController.getAllProducts(req, res);
   } catch (error) {
+    console.log(error)
     res.status(500).json({ error: error.toString() });
   }
 };
